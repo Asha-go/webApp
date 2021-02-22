dcf18ea8334652c65c436a890c2e38d86d1aa168
devdeng <devdeng@tencent.com>
2021-02-22 17:47:39 +0800

    up

--- pages/h5/serDetail/_id.vue
+++ pages/h5/serDetail/_id.vue
@@ -4,7 +4,7 @@
         <div> {{detail.location}}</div>
         <div> {{detail.price}}</div>
         <img :src="detail.image" alt="">
-        <div >{{detail.desc}}</div>
+        <div v-html="detail.desc"></div>
     </div>
 </template>
 <script>
@@ -19,16 +19,16 @@ export default {
         return {
             detail: {
                 title: 'chinese teacher',
-                location: '',
+                location: 'shang Hai',
                 price: '444/hr',
-                image: '//',
-                desc: ''
+                image: '//ashago-resource.oss-cn-zhangjiakou.aliyuncs.com/Asha%20Go%20Website%201.0/Articles/Language/movies%20to%20learn%20chinese/%E5%8E%9F%E5%9B%BE7.jpg',
+                desc: 'Watching movies is one of the best ways to improve your Chinese language and your understanding of Chinese history and culture. Luckily, there is a plethora of excellent Chinese films to choose from ranging from comedies, historical dramas to action-packed thrillers. We’ve selected some of the best lesser known Chinese films for you to browse. Are we missing your favourite Chinese film? Let us know in the comment section below! '
             },
-            
+
         }
     },
     methods: {
-        
+
     }
 }
 </script>
