7eed05f2ba401891631dc6c3ee6b1f1f6300bde7
devdeng <devdeng@tencent.com>
2021-04-05 21:20:37 +0800

    up

--- plugins/antd-ui.js
+++ plugins/antd-ui.js
@@ -26,10 +26,12 @@ import { Row } from 'ant-design-vue';
 import { List} from 'ant-design-vue';
 import { Tag } from 'ant-design-vue';
 import { Spin } from 'ant-design-vue';
+import { Select } from 'ant-design-vue';
 
 // All the component found in https://github.com/vueComponent/ant-design-vue/blob/master/components/index.js
 // import 'ant-design-vue/lib/button/style/css';
 Vue.use(Button);
+Vue.use(Select);
 Vue.use(Form);
 Vue.use(Input);
 Vue.use(Icon);
